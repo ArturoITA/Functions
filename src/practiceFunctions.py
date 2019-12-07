631f19f72c20e3bb346dcf105de45ecddc4514c0
ArturoITA <55923813+ArturoITA@users.noreply.github.com>
2019-12-07 11:11:03 -0600

    Initial commit

---
+++ src/practiceFunctions.py
@@ -0,0 +1,15 @@
+'''
+Created on Nov 16, 2019
+
+@author: ITAUser
+'''
+def addition(num1, num2):
+    sumOftwoNumbers = num1 +num2
+    return sumOftwoNumbers
+sumOftwoNumbers = addition(2,5)
+
+def sum_three_numbers(num1, num2, num3):
+    sumOfnumbers = num1 + num2 + num3 
+    return sumOfnumbers
+
+sumOfthreeNumbers = sum_three_numbers(5, 4, 1)
